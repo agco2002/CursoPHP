4036194731cde68e900d41d3d4cfc965c47793b0
agco2002 <agco2002@gmail.com>
2024-06-05 17:06:50 -0500

    Codigo PHP

---
+++ constantes.php
@@ -0,0 +1,14 @@
+<?php
+/*Las constantes siempre se escriben en mayusculas*/ 
+/*Programa para pasar pulgadas a cm  */
+define('PULGADA', 0.393701);
+define('LIBRA', 2.020462);
+
+$cm = 50;
+$kilo = 5;
+
+echo $cm * PULGADA . '<br>';
+
+
+
+?>
---
+++ variables.php
@@ -0,0 +1,20 @@
+
+<?php
+/* Tipos de datos de php: Escalares Compuestos  Especiales 
+Ejemplos: Boolean, Integer, float, string*/ 
+
+$nombre = "Jose";
+$edad = 20;
+$altura = 1.72;
+$casado = true;
+
+var_dump($nombre);
+echo '<br>';
+echo $edad;
+echo '<br>';
+echo $altura;
+echo '<br>';
+var_dump($casado);
+
+
+?>
